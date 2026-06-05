77a52394d37271da51294949ee89882cc3673e85
Joshi-hub <joshuaauerbach@web.de>
2026-06-05 16:30:00 +0200

    feat: setup core project, docker environment and video model signals

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_rq',
     'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
---
+++ core/signals.py
@@ -0,0 +1,29 @@
+from django.db.models.signals import post_save, post_delete
+from .models import Video
+from django.dispatch import receiver
+import os
+
+
+@receiver(post_save, sender=Video)
+def video_post_save(sender, instance, created, **kwargs):
+    print('video wurde gespeichert')
+    if created:
+        print('new video wurde gespeichert')
+
+
+@receiver(post_delete, sender=Video)
+def video_post_delete(sender, instance, **kwargs):
+    print('video wurde gelöscht')
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path): 
+            os.remove(instance.video_file.path)
+            print('video file wurde gelöscht')
+    if instance.thumbnail:
+        if os.path.isfile(instance.thumbnail.path):
+            os.remove(instance.thumbnail.path)  
+            print('thumbnail wurde gelöscht')
+
+
+
+
+       
