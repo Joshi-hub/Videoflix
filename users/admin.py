dbbbf0ed76317d6db6095cce03d7d54878ee520d
Joshi-hub <joshuaauerbach@web.de>
2026-06-08 11:00:00 +0200

    feat(users): add CustomUser model with email-based authentication
    - Define CustomUser with email as USERNAME_FIELD, inactive by default
    - Set AUTH_USER_MODEL, register in admin, add initial migration

---
+++ users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ['email', 'is_active', 'is_staff', 'created_at']
+    list_filter = ['is_active', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ['email']
+    ordering = ['email']
+    filter_horizontal = ('groups', 'user_permissions')
